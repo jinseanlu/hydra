8dbf7aa772e5209846c190c3c675b20385a04594
jinseanlu <117480992+jinseanlu@users.noreply.github.com>
2022-12-20 15:56:02 -0500

    Create hamster.js

---
+++ hamster.js
@@ -0,0 +1,12 @@
+//This is made by my little brother, not me!
+s0.initVideo("https://media3.giphy.com/media/pFonPqjcxtSEg/giphy.mp4")
+src(s0).out(o0)
+
+
+function voronoiShapes(sc, sp, bl){return voronoi(sc, sp, bl)}
+
+voronoiShapes()
+//voronoi(10,100,2).blend (vornoi(100, 100, 0.25))
+
+  .out(o0)
+render(o0)
